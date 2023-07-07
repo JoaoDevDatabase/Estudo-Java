47b26418f79a1617aee72eca231399c20df72bc1
João Vitor Stellet <joaovitorstellet@gmail.com>
2023-07-07 11:52:25 -0300

    new file:   BoletimEstudantil.java

---
+++ BoletimEstudantil.java
@@ -0,0 +1,29 @@
+// arquivo BoletimEstudantil.java
+// Um estudo para poder aprender else/if em java!
+
+package src;
+
+import java.util.Scanner;
+
+public class BoletimEstudantil {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int Number;
+
+        System.out.print("Insira um número: ");
+        Number = input.nextInt();
+
+        if (Number < 6) {
+            System.out.println("REPROVADO");
+        } else if (Number == 6) {
+            System.out.println("PASSOU RASPANDO");
+        } else {
+            System.out.println("PARABENS VOCE FOI APROVADO!");
+        }
+
+    }
+}
+// PROGRAMA FEITO COM SUCESSO !
